35249236579fd2caeef756932af46fd7e9484a9f
Stanislav Goremykin <sgoremyk@c2r13s2.42yerevan.am>
2023-11-26 11:32:52 +0400

    2

--- rush-01.c
+++ rush-01.c
@@ -1,31 +1,28 @@
 #include "unistd.h"
 #include "stdio.h"
 #include "stdlib.h"
+
 char	**ft_calloc(void);
 int		ft_strlen(char *str);
 int		validation(int argc, char **argv);
 void	init(char **map, char **argv);
-int	SIZE;
-int	main(int argc, char* argv[])
-{
-	int 	i;
-	char	**map;
-	
-	if (validation(argc, argv))
-		return (-1);
-	map = ft_calloc();
-	if (map == 0)
-		return (-1);
-	init(map, argv);
+int	g_size;
 
+int	main(int argc, char *argv[])
+{
+	int		i;
+	char	**map;
+
+	if (validation(argc, argv) || ((map = ft_calloc()) == 0))
+		return (-1);
 	i = -1;
-	while (++i < SIZE)
+	init(map, argv);
+	while (++i < g_size)
 	{
-		write(1, map[i], SIZE);
+		write(1, map[i], g_size);
 		write(1, "\n", 1);
 	}
-			
-	while (++i < SIZE && argc == 2)
+	while (++i < g_size)
 		free(map[i]);
 	free(map);
 	return (0);
@@ -36,12 +33,12 @@ char	**ft_calloc(void)
 	char	**map;
 	int		i;
 	int		j;
-	
-	map = (char**)malloc(SIZE * sizeof(char*));
+
+	map = (char **)malloc(g_size * sizeof(char *));
 	i = -1;
-	while (++i < SIZE)
+	while (++i < g_size)
 	{
-		map[i] = (char*)malloc(SIZE);
+		map[i] = (char *)malloc(g_size);
 		if (map == 0 || map[i] == 0)
 		{
 			write(2, "No memory!\n", 11);
@@ -49,10 +46,10 @@ char	**ft_calloc(void)
 		}
 	}
 	i = -1;
-	while (++i < SIZE)
+	while (++i < g_size)
 	{
 		j = -1;
-		while (++j < SIZE)
+		while (++j < g_size)
 			map[i][j] = '0';
 	}
 	return (map);
@@ -64,30 +61,30 @@ void	init(char **map, char **argv)
 	int	j;
 
 	i = 0;
-	while (i < SIZE)
+	while (i < g_size)
 	{
 		j = 0;
-		while (++j < SIZE-1)
+		while (++j < g_size - 1)
 		{
 			if (*argv[1] == ' ')
 				argv[1]++;
 			map[i][j] = *argv[1];
 			argv[1]++;
 		}
-		i += SIZE - 1;
+		i += g_size - 1;
 	}
 	j = 0;
 	while (*argv[1])
 	{
 		i = 0;
-		while (++i < SIZE - 1)
+		while (++i < g_size - 1)
 		{
 			if (*argv[1] == ' ')
 				argv[1]++;
 			map[i][j] = *argv[1];
 			argv[1]++;
 		}
-		j += SIZE - 1;
+		j += g_size - 1;
 	}
 }
 
@@ -102,17 +99,19 @@ int	validation(int argc, char **argv)
 	i = 0;
 	if (len == 31)
 	{
-		while (argv[1][i] >= '1' && argv[1][i] <= '4' && argv[1][i+1] == ' ')
+		while (argv[1][i] >= '1' && argv[1][i] <= '4'
+			&& (argv[1][i + 1] == ' ' || argv[1][i + 1] == '\0'))
 			i += 2;
-		SIZE = 6;
+		g_size = 6;
 	}
 	else if (len == 71)
 	{
-		while (argv[1][i] >= '1' && argv[1][i] <= '9' && argv[1][i+1] == ' ')
+		while (argv[1][i] >= '1' && argv[1][i] <= '9'
+			&& (argv[1][i + 1] == ' ' || argv[1][i + 1] == '\0'))
 			i += 2;
-		SIZE = 11;
+		g_size = 11;
 	}
-	if (i == len - 1)
+	if (i == len + 1)
 		return (0);
 	write(2, "Incorrect argument\n", 19);
 	return (-1);
